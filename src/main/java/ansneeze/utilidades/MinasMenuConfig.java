cd30983e07c76b54ce58230f7ff993e33e297d72
xmargnez <mragestudio@gmail.com>
2026-03-17 11:32:33 -0300

    asd

---
+++ src/main/java/ansneeze/utilidades/MinasMenuConfig.java
@@ -0,0 +1,43 @@
+package ansneeze.utilidades;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MinasMenuConfig {
+    private final JavaPlugin plugin;
+    private final File minasFile;
+    private FileConfiguration config;
+
+    public MinasMenuConfig(JavaPlugin plugin) {
+        this.plugin = plugin;
+        minasFile = new File(plugin.getDataFolder(), "minasmenus.yml");
+        if (!minasFile.exists()) {
+            try {
+                minasFile.createNewFile();
+                config = YamlConfiguration.loadConfiguration(minasFile);
+                config.set("minas", null);
+                save();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        config = YamlConfiguration.loadConfiguration(minasFile);
+        if (!config.contains("minas")) config.set("minas", null);
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+
+    public void save() {
+        try {
+            config.save(minasFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
